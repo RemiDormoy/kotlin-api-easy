f437c5374ecc30639e494e8c0b227de52281cb40
Remi Dormoy <rdormoy@octo.com>
2019-07-17 17:05:55 +0200

    added error treatment

--- src/main/java/api/bootstrap/players/PlayersController.kt
+++ src/main/java/api/bootstrap/players/PlayersController.kt
@@ -1,9 +1,10 @@
 package api.bootstrap.players
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import java.time.format.DateTimeFormatter
 
 @RestController
@@ -31,3 +32,19 @@ class PlayersController {
         val nationality: String
     )
 }
+
+@ControllerAdvice(assignableTypes = [PlayersController::class])
+class PlayersControllerAdvice {
+
+    @ExceptionHandler(TeamNotFoundException::class)
+    fun invalidParams(exc: TeamNotFoundException): ResponseEntity<ApiError> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(ApiError("No team found for this id : ${exc.id}"))
+
+    }
+}
+
+data class ApiError(
+    val message: String
+)
